e4090eadd6c2b554bd98c53fd2e41c142c164f90
Aidan Schulmeister <sniperninja564@gmail.com>
2019-10-16 15:19:47 -0700

    main

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Katam_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.p1Kracko = new System.Windows.Forms.Panel();
+            this.p1KG = new System.Windows.Forms.Panel();
+            this.p1Gobbler = new System.Windows.Forms.Panel();
+            this.p1Moley = new System.Windows.Forms.Panel();
+            this.p1Dmk = new System.Windows.Forms.Panel();
+            this.p1MT = new System.Windows.Forms.Panel();
+            this.p1Wiz = new System.Windows.Forms.Panel();
+            this.p1Hand = new System.Windows.Forms.Panel();
+            this.p2Hand = new System.Windows.Forms.Panel();
+            this.p2Wiz = new System.Windows.Forms.Panel();
+            this.p2MT = new System.Windows.Forms.Panel();
+            this.p2Dmk = new System.Windows.Forms.Panel();
+            this.p2Moley = new System.Windows.Forms.Panel();
+            this.p2Gobbler = new System.Windows.Forms.Panel();
+            this.p2KG = new System.Windows.Forms.Panel();
+            this.p2Kracko = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // p1Kracko
+            // 
+            this.p1Kracko.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p1Kracko.Location = new System.Drawing.Point(128, 16);
+            this.p1Kracko.Name = "p1Kracko";
+            this.p1Kracko.Size = new System.Drawing.Size(96, 96);
+            this.p1Kracko.TabIndex = 0;
+            this.p1Kracko.Click += new System.EventHandler(this.p1Kracko_Click);
+            // 
+            // p1KG
+            // 
+            this.p1KG.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p1KG.Location = new System.Drawing.Point(240, 16);
+            this.p1KG.Name = "p1KG";
+            this.p1KG.Size = new System.Drawing.Size(96, 96);
+            this.p1KG.TabIndex = 1;
+            this.p1KG.Click += new System.EventHandler(this.p1KG_Click);
+            // 
+            // p1Gobbler
+            // 
+            this.p1Gobbler.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p1Gobbler.Location = new System.Drawing.Point(352, 72);
+            this.p1Gobbler.Name = "p1Gobbler";
+            this.p1Gobbler.Size = new System.Drawing.Size(96, 96);
+            this.p1Gobbler.TabIndex = 2;
+            this.p1Gobbler.Click += new System.EventHandler(this.p1Gobbler_Click);
+            // 
+            // p1Moley
+            // 
+            this.p1Moley.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p1Moley.Location = new System.Drawing.Point(352, 184);
+            this.p1Moley.Name = "p1Moley";
+            this.p1Moley.Size = new System.Drawing.Size(96, 96);
+            this.p1Moley.TabIndex = 3;
+            this.p1Moley.Click += new System.EventHandler(this.p1Moley_Click);
+            // 
+            // p1Dmk
+            // 
+            this.p1Dmk.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p1Dmk.Location = new System.Drawing.Point(240, 240);
+            this.p1Dmk.Name = "p1Dmk";
+            this.p1Dmk.Size = new System.Drawing.Size(96, 96);
+            this.p1Dmk.TabIndex = 4;
+            this.p1Dmk.Click += new System.EventHandler(this.p1Dmk_Click);
+            // 
+            // p1MT
+            // 
+            this.p1MT.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p1MT.Location = new System.Drawing.Point(128, 240);
+            this.p1MT.Name = "p1MT";
+            this.p1MT.Size = new System.Drawing.Size(96, 96);
+            this.p1MT.TabIndex = 5;
+            this.p1MT.Click += new System.EventHandler(this.p1MT_Click);
+            // 
+            // p1Wiz
+            // 
+            this.p1Wiz.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p1Wiz.Location = new System.Drawing.Point(16, 184);
+            this.p1Wiz.Name = "p1Wiz";
+            this.p1Wiz.Size = new System.Drawing.Size(96, 96);
+            this.p1Wiz.TabIndex = 6;
+            this.p1Wiz.Click += new System.EventHandler(this.p1Wiz_Click);
+            // 
+            // p1Hand
+            // 
+            this.p1Hand.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p1Hand.Location = new System.Drawing.Point(16, 72);
+            this.p1Hand.Name = "p1Hand";
+            this.p1Hand.Size = new System.Drawing.Size(96, 96);
+            this.p1Hand.TabIndex = 7;
+            this.p1Hand.Click += new System.EventHandler(this.p1Hand_Click_1);
+            // 
+            // p2Hand
+            // 
+            this.p2Hand.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p2Hand.Location = new System.Drawing.Point(480, 72);
+            this.p2Hand.Name = "p2Hand";
+            this.p2Hand.Size = new System.Drawing.Size(96, 96);
+            this.p2Hand.TabIndex = 15;
+            this.p2Hand.Click += new System.EventHandler(this.p2Hand_Click);
+            // 
+            // p2Wiz
+            // 
+            this.p2Wiz.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p2Wiz.Location = new System.Drawing.Point(480, 184);
+            this.p2Wiz.Name = "p2Wiz";
+            this.p2Wiz.Size = new System.Drawing.Size(96, 96);
+            this.p2Wiz.TabIndex = 14;
+            this.p2Wiz.Click += new System.EventHandler(this.p2Wiz_Click);
+            // 
+            // p2MT
+            // 
+            this.p2MT.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p2MT.Location = new System.Drawing.Point(592, 240);
+            this.p2MT.Name = "p2MT";
+            this.p2MT.Size = new System.Drawing.Size(96, 96);
+            this.p2MT.TabIndex = 13;
+            this.p2MT.Click += new System.EventHandler(this.p2MT_Click);
+            // 
+            // p2Dmk
+            // 
+            this.p2Dmk.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p2Dmk.Location = new System.Drawing.Point(704, 240);
+            this.p2Dmk.Name = "p2Dmk";
+            this.p2Dmk.Size = new System.Drawing.Size(96, 96);
+            this.p2Dmk.TabIndex = 12;
+            this.p2Dmk.Click += new System.EventHandler(this.p2Dmk_Click);
+            // 
+            // p2Moley
+            // 
+            this.p2Moley.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p2Moley.Location = new System.Drawing.Point(816, 184);
+            this.p2Moley.Name = "p2Moley";
+            this.p2Moley.Size = new System.Drawing.Size(96, 96);
+            this.p2Moley.TabIndex = 11;
+            this.p2Moley.Click += new System.EventHandler(this.p2Moley_Click);
+            // 
+            // p2Gobbler
+            // 
+            this.p2Gobbler.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p2Gobbler.Location = new System.Drawing.Point(816, 72);
+            this.p2Gobbler.Name = "p2Gobbler";
+            this.p2Gobbler.Size = new System.Drawing.Size(96, 96);
+            this.p2Gobbler.TabIndex = 10;
+            this.p2Gobbler.Click += new System.EventHandler(this.p2Gobbler_Click);
+            // 
+            // p2KG
+            // 
+            this.p2KG.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p2KG.Location = new System.Drawing.Point(704, 16);
+            this.p2KG.Name = "p2KG";
+            this.p2KG.Size = new System.Drawing.Size(96, 96);
+            this.p2KG.TabIndex = 9;
+            this.p2KG.Click += new System.EventHandler(this.p2KG_Click);
+            // 
+            // p2Kracko
+            // 
+            this.p2Kracko.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.p2Kracko.Location = new System.Drawing.Point(592, 16);
+            this.p2Kracko.Name = "p2Kracko";
+            this.p2Kracko.Size = new System.Drawing.Size(96, 96);
+            this.p2Kracko.TabIndex = 8;
+            this.p2Kracko.Click += new System.EventHandler(this.p2Kracko_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Katam_Tracker.Properties.Resources.main1;
+            this.ClientSize = new System.Drawing.Size(928, 352);
+            this.Controls.Add(this.p2Hand);
+            this.Controls.Add(this.p1Hand);
+            this.Controls.Add(this.p2Wiz);
+            this.Controls.Add(this.p1Wiz);
+            this.Controls.Add(this.p2MT);
+            this.Controls.Add(this.p1MT);
+            this.Controls.Add(this.p2Dmk);
+            this.Controls.Add(this.p1Dmk);
+            this.Controls.Add(this.p2Moley);
+            this.Controls.Add(this.p1Moley);
+            this.Controls.Add(this.p2Gobbler);
+            this.Controls.Add(this.p1Gobbler);
+            this.Controls.Add(this.p2KG);
+            this.Controls.Add(this.p1KG);
+            this.Controls.Add(this.p2Kracko);
+            this.Controls.Add(this.p1Kracko);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Katam Tracker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel p1Kracko;
+        private System.Windows.Forms.Panel p1KG;
+        private System.Windows.Forms.Panel p1Gobbler;
+        private System.Windows.Forms.Panel p1Moley;
+        private System.Windows.Forms.Panel p1Dmk;
+        private System.Windows.Forms.Panel p1MT;
+        private System.Windows.Forms.Panel p1Wiz;
+        private System.Windows.Forms.Panel p1Hand;
+        private System.Windows.Forms.Panel p2Hand;
+        private System.Windows.Forms.Panel p2Wiz;
+        private System.Windows.Forms.Panel p2MT;
+        private System.Windows.Forms.Panel p2Dmk;
+        private System.Windows.Forms.Panel p2Moley;
+        private System.Windows.Forms.Panel p2Gobbler;
+        private System.Windows.Forms.Panel p2KG;
+        private System.Windows.Forms.Panel p2Kracko;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Katam_Tracker {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            p1Kracko.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p1KG.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p1Gobbler.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p1Moley.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p1Dmk.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p1MT.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p1Wiz.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p1Hand.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p2Kracko.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p2KG.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p2Gobbler.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p2Moley.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p2Dmk.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p2MT.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p2Wiz.BackColor = Color.FromArgb(128, 0, 0, 0);
+            p2Hand.BackColor = Color.FromArgb(128, 0, 0, 0);
+        }
+
+        // Player 1
+        private void p1Kracko_Click(object sender, EventArgs e) {
+            if (p1Kracko.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p1Kracko.BackColor = Color.FromArgb(128, 0, 0, 0);
+            } else {
+                p1Kracko.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p1KG_Click(object sender, EventArgs e) {
+            if (p1KG.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p1KG.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p1KG.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p1Gobbler_Click(object sender, EventArgs e) {
+            if (p1Gobbler.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p1Gobbler.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p1Gobbler.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p1Moley_Click(object sender, EventArgs e) {
+            if (p1Moley.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p1Moley.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p1Moley.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p1Dmk_Click(object sender, EventArgs e) {
+            if (p1Dmk.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p1Dmk.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p1Dmk.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p1MT_Click(object sender, EventArgs e) {
+            if (p1MT.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p1MT.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p1MT.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p1Wiz_Click(object sender, EventArgs e) {
+            if (p1Wiz.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p1Wiz.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p1Wiz.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p1Hand_Click_1(object sender, EventArgs e) {
+            if (p1Hand.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p1Hand.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p1Hand.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        // Player 2
+        private void p2Kracko_Click(object sender, EventArgs e) {
+            if (p2Kracko.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p2Kracko.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p2Kracko.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p2KG_Click(object sender, EventArgs e) {
+            if (p2KG.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p2KG.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p2KG.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p2Gobbler_Click(object sender, EventArgs e) {
+            if (p2Gobbler.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p2Gobbler.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p2Gobbler.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p2Moley_Click(object sender, EventArgs e) {
+            if (p2Moley.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p2Moley.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p2Moley.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p2Dmk_Click(object sender, EventArgs e) {
+            if (p2Dmk.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p2Dmk.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p2Dmk.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p2MT_Click(object sender, EventArgs e) {
+            if (p2MT.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p2MT.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p2MT.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p2Wiz_Click(object sender, EventArgs e) {
+            if (p2Wiz.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p2Wiz.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p2Wiz.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+        private void p2Hand_Click(object sender, EventArgs e) {
+            if (p2Hand.BackColor == Color.FromArgb(0, 0, 0, 0)) {
+                p2Hand.BackColor = Color.FromArgb(128, 0, 0, 0);
+            }
+            else {
+                p2Hand.BackColor = Color.FromArgb(0, 0, 0, 0);
+            }
+        }
+    }
+}
